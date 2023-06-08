2d7a2748bea09776628a3b182d4b6fcf4ac5cf71
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-06-08 01:53:34 -0300

    using styled components to learn about the "css in js" concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }
