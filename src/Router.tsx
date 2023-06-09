9f357150f5521865b7e4c23a13d66568d6f67e08
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-06-08 21:18:41 -0300

    creating the DefaultLayout route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 import { Home } from './pages/Home'
 import { History } from './pages/History'
@@ -6,8 +7,10 @@ import { History } from './pages/History'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
