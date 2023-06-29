feef5ae020eaec562251d5a5943122c6997ef6c6
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-06-29 01:19:18 -0300

    changing suggestions

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -19,9 +19,9 @@ export function NewCycleForm() {
       />
 
       <datalist id="task-suggestions">
-        <option value="Projeto 1" />
-        <option value="Projeto 2" />
-        <option value="Projeto 3" />
+        <option value="Estudar para prova" />
+        <option value="Limpar a casa" />
+        <option value="Estudar sobre monolitos" />
         <option value="Banana" />
       </datalist>
 
